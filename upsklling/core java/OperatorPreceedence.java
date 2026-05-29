466aaa07f9fc4f7df3be51e4c40c691710aca19b
Kaviya2005912 <kaviyavardhini648@gmail.com>
2026-05-29 21:49:54 +0530

    operator

---
+++ upsklling/core java/OperatorPreceedence.java
@@ -0,0 +1,13 @@
+public class OperatorPreceedence {
+    public static void main(String[] args) {
+        int a = 10, b = 5, c = 2;
+        int op = a + b * c; 
+        System.out.println("Result of a + b * c: " + op);
+        op = (a + b) * c; 
+        System.out.println("Result of (a + b) * c: " + op);
+        op = a - b / c; 
+        System.out.println("Result of a - b / c: " + op);
+        op = (a - b) / c; 
+        System.out.println("Result of (a - b) / c: " + op);
+    }
+}
